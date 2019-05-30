8092f16951bd62b6aa9c1579cc779ad406f72bc1
dancopeland <daniel.i.copeland@vanderbilt.edu>
2019-05-30 16:59:06 -0400

    still working, cleaned up columns, next is to remove deadbeats

--- fide.R
+++ fide.R
@@ -9,15 +9,42 @@ str(FIDE)
 
 head(FIDE)
 FIDE <- FIDE[-1,] # removed bad column
-head(FIDE)
+rownames(FIDE)<- NULL
+head(FIDE,75)
 
-# clean ratings columns
+# remove excess columns
 
+FIDE$SGm <- NULL
+FIDE$SK <- NULL
+FIDE$RGm <- NULL
+FIDE$Flag <- NULL
+
+# change col names
+colnames(FIDE)[colnames(FIDE) == "ID.Number"] <- "ID"
+colnames(FIDE)[colnames(FIDE) == "Tit..WTit.OTit"] <- "Title"
+colnames(FIDE)[colnames(FIDE) == "FOA.SRtng"] <- "SRting"
+colnames(FIDE)[colnames(FIDE) == "B.day"] <- "Birth_year"
+colnames(FIDE)[colnames(FIDE) == "RRtng"] <- "RRting"
+colnames(FIDE)[colnames(FIDE) == "Rk.BRtng.BGm.BK"] <- "BRting"
 
 # remove players who do not have any ratings
 
+#create boolean matrix for each rating type
 
+SR_bool <- NULL  # Check for blank
+RR_bool <- NULL  # Check for NA
+BR_bool <- NULL  # Check for blank
 
+FIDE$RRting[5]
+
+NA_SRting <- is.na(FIDE$SRting)
+
+anyNA(FIDE$ID.Number)
+
+any(FIDE$RRting, rm.na = TRUE)
+
+head(NA_SRting)
+# clean ratings columns
 
 
 
